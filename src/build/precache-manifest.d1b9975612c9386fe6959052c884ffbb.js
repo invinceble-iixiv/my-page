e18db702f8483868e57c8125e6726e2171dbd5df
Vince <vincent.taracatac@awsys-i.com>
2020-07-21 00:20:22 +0800

    Initial commit

---
+++ src/build/precache-manifest.d1b9975612c9386fe6959052c884ffbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6231b15d4dca1b9551eeaa86f336970",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b98b3649f927195a16a4",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "dc7ad11d9e99a0ce04af",
+    "url": "/static/js/2.73bda40f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.73bda40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b98b3649f927195a16a4",
+    "url": "/static/js/main.78f8d2b9.chunk.js"
+  },
+  {
+    "revision": "5e085f63831bab725976",
+    "url": "/static/js/runtime-main.285894c7.js"
+  }
+]);
